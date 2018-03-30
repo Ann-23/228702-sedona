e821a8a56e15cb0d2958605b24f938a9bbba49eb
Ann <anuta.gradna@yandex.ru>
2018-03-30 19:19:22 +0300

    Final_1

--- js/js.js
+++ js/js.js
@@ -1,8 +1,10 @@
  var link = document.querySelector(".interested-in-button");
-        
+ 
  var popup = document.querySelector(".modal");
-        
- link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle("modal-show");
-});
+ 
+ 	link.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		popup.classList.toggle("modal-show");
+	});
+	
+	
