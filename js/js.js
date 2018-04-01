37e709afd449c6ffa1f85ea6497b857f66eb87a5
Ann <anuta.gradna@yandex.ru>
2018-04-01 19:38:26 +0300

    Final_5

--- js/js.js
+++ js/js.js
@@ -1,13 +1,18 @@
- var link = document.querySelector(".interested-in-button");
- 
- var popup = document.querySelector(".modal");
- 
- 	link.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		if (popup.classList.contains("modal-open")) {
-        popup.classList.remove("modal-open");
-        popup.classList.add("modal-close");
-      } else{
-	  	popup.classList.toggle("modal-show");
-	  }
-    });
+var no_js = document.querySelector(".no-js");
+var form = popup.querySelector("interested-in-form");
+var link = document.querySelector(".interested-in-button");
+var popup = document.querySelector(".modal");
+
+if (no_js) {
+    document.body.classList.remove("no-js");
+}
+
+link.addEventListener("click", function (evt) {
+     evt.preventDefault();
+     if (popup.classList.contains("modal-open")) {
+         popup.classList.remove("modal-open");
+         popup.classList.add("modal-close");
+     } else {
+         popup.classList.toggle("modal-show");
+    }
+ });
